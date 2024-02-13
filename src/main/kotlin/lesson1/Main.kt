241f2e4dea94fa5ba95c194b98564664962aca70
Blynchik <vadimsovetnikov@mail.ru>
2024-02-13 17:14:12 +0300

    lesson1 & lesson2

---
+++ src/main/kotlin/lesson1/Main.kt
@@ -0,0 +1,16 @@
+package lesson1
+
+fun main() {
+    //переменные
+    var a: Int = 5
+    var b: Int = 3
+    var s: Int = a * b
+    println(s)
+    s = 10
+    println(s)
+
+    var name: String = "rectangle"
+    var diagonal: Double
+
+    val c: Int = 30
+}
---
+++ src/main/kotlin/lesson2/main.kt
@@ -0,0 +1,8 @@
+package lesson2
+
+fun main (){
+    //ввод данных с консоли
+    val console1: Int = readln().toInt()
+    val console2: Int = readln().toInt();
+    println(console1 * console2)
+}
